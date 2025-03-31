7e59382a2e6537915188bed7fba76f85c2cca0d5
kristoffer <kunkristoffer@gmail.com>
2025-03-31 12:27:16 +0200

    removing env support as its can cause some troubles when deploying studios

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,31 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Template project',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production" as string,
-  plugins: [structureTool(), visionTool()],
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    title: 'Template Project Live',
+    subtitle: 'production',
+    name: 'production-workspace',
+    basePath: '/production',
+    projectId: '<projectID>',
+    dataset: 'production',
+    plugins: [structureTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    title: 'Template Project Preview',
+    subtitle: 'development',
+    name: 'development-workspace',
+    basePath: '/development',
+    projectId: '<projectID>',
+    dataset: 'development',
+    plugins: [structureTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
